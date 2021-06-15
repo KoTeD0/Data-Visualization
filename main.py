0dd4589959e226e809444e45f317349406d41753
KoTeDo <darkest.jerry@gmail.com>
2021-06-15 14:14:05 +0300

    fonts changes and title addition

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# название заголовка диаграммы и меток осей
+ax.set_title("Square Numbers", fontsize=22)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# назначение размера шрифта делейний на осях
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
