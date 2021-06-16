00a9fc12ea0bcaf4e121320f427b6940e1a97830
KoTeDo <darkest.jerry@gmail.com>
2021-06-16 21:44:44 +0300

    added scatters color map

--- scatter_squares.py
+++ scatter_squares.py
@@ -5,7 +5,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Назначение заголовка диаграммы и меток осей
 ax.set_title("Square Numbers", fontsize=24)
@@ -15,7 +15,7 @@ ax.set_ylabel("Square of Value", fontsize=14)
 # назначение размера шрифта делелний и осях
 ax.tick_params(axis='both', which='major', labelsize=14)
 
-# назначение дипапозона для каждой оси
+# назначение дипапозона для каждой оси.
 ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
