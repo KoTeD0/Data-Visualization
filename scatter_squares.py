927aa054f47062fe874ddccc1c7cf009965a1a39
KoTeDo <darkest.jerry@gmail.com>
2021-06-16 18:50:42 +0300

    added more scatters

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, s=10)
 
 # Назначение заголовка диаграммы и меток осей
 ax.set_title("Square Numbers", fontsize=24)
@@ -11,5 +14,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # назначение размера шрифта делелний и осях
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+# назначение дипапозона для каждой оси
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
