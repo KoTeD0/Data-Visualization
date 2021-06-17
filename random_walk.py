b1fbc5d6b5e4eaf3c82452ce2f79b2a1d6622566
KoTeDo <darkest.jerry@gmail.com>
2021-06-17 21:50:49 +0300

    refactored Random walk, and updated rw_visual

--- random_walk.py
+++ random_walk.py
@@ -3,12 +3,27 @@ from random import choice
 class RandomWalk:
     """Класс для генерирования случайных блужданий"""
 
-    def __init__(self, num_points=5000):
+    def __init__(self, num_points=5000000):
         """Инициалиирует атрибуты блужданий"""
         self.num_points = num_points
 
         self.x_values = [0]
         self.y_values = [0]
+
+    def get_step(self):
+        # Определение направления и длинны перемещения:
+        x_direction = choice([-1, 1])
+        x_distance = choice([0, 1, 2, 3, 4])
+        x_step = x_direction * x_distance
+
+        y_direction = choice([-1, 1])
+        y_distance = choice([0, 1, 2, 3, 4])
+        y_step = y_direction * y_distance
+
+        # Отклонение нулевых перемещений
+        if x_step == 0 and y_step == 0:
+            self.get_step()
+        return x_step, y_step
 
     def fill_walk(self):
         """Вычисляет все точки блужданий"""
@@ -16,22 +31,11 @@ class RandomWalk:
         # Шаги генерируются до достижения нужной длинны
         while len(self.x_values) < self.num_points:
 
-            # Определение направления и длинны перемещения:
-            x_direction = choice([-1, 1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([-1, 1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
-
-            # Отклонение нулевых перемещений
-            if x_step == 0 and y_step == 0:
-                continue
+            next_step = self.get_step()
 
             # вычисление следующих значений x и y
-            x = self.x_values[-1] + x_step
-            y = self.y_values[-1] + y_step
+            x = self.x_values[-1] + next_step[0]
+            y = self.y_values[-1] + next_step[1]
 
             self.x_values.append(x)
             self.y_values.append(y)
--- rw_visual.py
+++ rw_visual.py
@@ -2,12 +2,26 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Построение случайного блуждания
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    # Построение случайного блуждания
+    rw = RandomWalk(50000)
+    rw.fill_walk()
 
-# Нанесение точек на диаграмму
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    # Нанесение точек на диаграмму
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(16, 9), dpi=108)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Oranges, edgecolors='none', s=1,)
+
+    # Выделение первой и последней точки
+    ax.scatter(0, 0, c='green', edgecolor='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none', s=100)
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running.lower() == 'n':
+        break
