722f1104407a85229c093ab0715a3b0a3fd798fc
KoTeDo <darkest.jerry@gmail.com>
2021-06-20 15:49:39 +0300

    some functionality added

--- dice_visual.py
+++ dice_visual.py
@@ -4,10 +4,10 @@ from plotly import offline
 from dice import Dice
 
 dice_1 = Dice()
-dice_2 = Dice()
+dice_2 = Dice(10)
 
 results = []
-for roll_num in range(1000):
+for roll_num in range(50000):
     results.append(dice_1.roll() + dice_2.roll())
 
 # Анализ результата
@@ -22,5 +22,5 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Result of Rolling two D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+my_layout = Layout(title='Result of Rolling D6 and D10 50000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
