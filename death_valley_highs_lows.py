2ca6ea8d482c2567bc51bb488cafcff9681fff74
KoTeDo <darkest.jerry@gmail.com>
2021-06-22 00:32:19 +0300

    death valley Temperatures

---
+++ death_valley_highs_lows.py
@@ -0,0 +1,46 @@
+import csv
+
+from matplotlib import pyplot as plt
+from datetime import datetime
+
+
+def makeCelsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+
+filename = 'data/death_valley_2018_simple.csv'
+with open(filename) as file:
+    reader = csv.reader(file)
+    header_row = next(reader)
+
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(makeCelsius(high))
+            lows.append(makeCelsius(low))
+
+print(dates, lows, highs)
+
+# Нанесение данных на диаграмму
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=1)
+ax.plot(dates, lows, c='blue', alpha=1)
+plt.fill_between(dates, highs, lows, facecolor='aqua', alpha=0.1)
+
+# Форматирование диаграммы
+title = "Daily High and Low temperatures - 2018\nDeath Valley, CA"
+plt.title(title, fontsize=20)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (C)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
